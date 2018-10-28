accbe5b9408dc02d72419e14adcd5067aa7fe163
Artur Wozniak <wozniaka83@gmail.com>
2018-10-28 12:58:51 +0100

    html order for rwd, rwd selector, js for rwd

--- js/script.js
+++ js/script.js
@@ -1,3 +1,42 @@
-window.onload = function() {
-  
+
+let winWidth;
+let headingMain = document.querySelector('#heading-main');
+let rowClassItem = document.querySelectorAll('.row');
+let row1Right = document.querySelector('.row1-right');
+let row2Left = document.querySelector('.row2-left');
+
+
+window.onresize = function(e){
+  winWidth = this.innerWidth;
+  console.log(winWidth);
+  if (winWidth <= 768 ) {
+    removeItem(row1Right);
+    removeItem(row2Left);
+    prependItem(rowClassItem[2],row1Right);
+    appendItem(rowClassItem[1], row2Left);    
+  } else {
+    removeItem(row1Right);
+    removeItem(row2Left);
+    prependItem(rowClassItem[2],row2Left);
+    appendItem(rowClassItem[1], row1Right);
+  } 
 }
+
+//Remove items from row
+function removeItem(item) {
+  item.remove();
+}
+
+
+// prepend append items to change order
+function prependItem(parent, itemPrep) {
+  parent.prepend(itemPrep);
+}
+function appendItem(parent, itemApp) {
+  parent.append(itemApp);
+}
+
+
+
+  
+
