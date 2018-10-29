191041d085f797f13297171342270e2df145743f
Artur Wozniak <wozniaka83@gmail.com>
2018-10-29 08:19:21 +0100

    changeOrder function, onload, window width basic value

--- js/script.js
+++ js/script.js
@@ -1,14 +1,31 @@
-
-let winWidth;
 let headingMain = document.querySelector('#heading-main');
 let rowClassItem = document.querySelectorAll('.row');
 let row1Right = document.querySelector('.row1-right');
 let row2Left = document.querySelector('.row2-left');
+let winWidth = innerWidth;
 
-
-window.onresize = function(e){
-  winWidth = this.innerWidth;
-  console.log(winWidth);
+// onresize
+window.onresize = function(e){  
+  winWidth = e.target.innerWidth;  
+  changeOrder(); 
+}
+// onload
+window.onload = function() {  
+  changeOrder();
+}
+//Remove items from row
+function removeItem(item) {
+  item.remove();
+}
+// prepend append items to change order
+function prependItem(parent, itemPrep) {
+  parent.prepend(itemPrep);
+}
+function appendItem(parent, itemApp) {
+  parent.append(itemApp);
+}
+// change order function
+function changeOrder() {
   if (winWidth <= 768 ) {
     removeItem(row1Right);
     removeItem(row2Left);
@@ -19,22 +36,9 @@ window.onresize = function(e){
     removeItem(row2Left);
     prependItem(rowClassItem[2],row2Left);
     appendItem(rowClassItem[1], row1Right);
-  } 
+  }
 }
 
-//Remove items from row
-function removeItem(item) {
-  item.remove();
-}
-
-
-// prepend append items to change order
-function prependItem(parent, itemPrep) {
-  parent.prepend(itemPrep);
-}
-function appendItem(parent, itemApp) {
-  parent.append(itemApp);
-}
 
 
 
